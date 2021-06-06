b506d2df8c36bb85559c9ea14e2504da208f6fa7
jonellsieren <70969803+jonellsieren@users.noreply.github.com>
2021-06-06 08:46:04 -0500

    Initial commit

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => (
+<>
+<h1>About Me</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget felis fringilla lectus efficitur finibus. Sed ut tincidunt arcu, non sagittis dolor. Curabitur arcu nibh, bibendum ac libero a, laoreet egestas velit. Proin semper id diam sit amet imperdiet. Suspendisse vitae cursus lorem. Vivamus eu lobortis lacus. Curabitur sem felis, mollis nec risus sit amet, ultricies aliquam augue. Etiam facilisis, orci in fringilla ultrices, odio libero euismod nisi, sed facilisis ex ipsum non tellus. Vestibulum dignissim ante sit amet erat bibendum, ut sollicitudin metus commodo. Praesent porttitor hendrerit placerat. In id fermentum turpis. Vestibulum eleifend libero nunc, in rhoncus quam accumsan interdum. Pellentesque eros ex, finibus id augue et, auctor maximus purus. Nunc sed finibus odio. Aliquam commodo fermentum nibh auctor pulvinar. Fusce urna ante, sollicitudin eu placerat sed, maximus vitae eros.</p>
+<p>Suspendisse at tincidunt massa. Vestibulum lectus sapien, fermentum eget rutrum quis, vehicula quis tortor. Ut ultrices sollicitudin aliquet. Nullam ex nulla, tristique sed cursus nec, sollicitudin ut eros. Aliquam lectus erat, consectetur a bibendum sed, feugiat ut lacus. Aenean sed ullamcorper justo, ac posuere est. Vivamus ac pretium urna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vulputate convallis purus, sit amet accumsan quam ultricies quis. Vestibulum rutrum viverra enim, in commodo augue. Nam id erat dictum, iaculis nunc sit amet, blandit eros. Cras vel lectus maximus, accumsan neque et, suscipit justo.</p>
+</>
+);
+
+export default AboutPage;
---
+++ src/pages/ArticlesListPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ArticlesList from '../components/ArticlesList';
+import articleContent from './article-content';
+
+const ArticlesListPage = () => (
+<>
+<h1>Article List</h1>
+<ArticlesList articles={articleContent} />
+    
+
+</>
+);
+
+export default ArticlesListPage;
